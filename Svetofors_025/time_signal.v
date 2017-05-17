9453e59654a797dea90211b6467da22045f7d06a
Alexander Kirichenko <aakirichenko@gmail.com>
2017-05-17 19:36:56 +0500

    Svetofors_025 -add few english comments -compile the project

--- Svetofors_025/time_signal.v
+++ Svetofors_025/time_signal.v
@@ -1,32 +1,43 @@
-module time_signal ( //This module make a signal with half-second period of time.
+module time_signal ( //Этот модуль описывает генератор сигнала времени, с периодом в пол-секунды.
+							//This module make a signal with half-second period of time.
 		input wire clk,
 		input wire reset,
 		output wire time_out
 		);
 	
-	reg [24:0] clk_number; //Clock counter, Waveshare CoreEP4CE10 have 50MHz global clk. Need just half.
-	reg [0:0] sec_kp; //A signal keeper.
-	
-	always @(posedge clk)
+	reg [24:0] clk_number; //Счетчик тактов, на Waveshare CoreEP4CE10 установлен источник 50МГц сигнала.
+								  //Clock counter, Waveshare CoreEP4CE10 have 50MHz global clk. Need just half.
+	reg [0:0] sec_kp;		  //Для хранения значения выходного сигнала между отсчетами.
+								  //A signal keeper.
+								  
+	assign time_out = sec_kp; //Соединение выходного провода time_out модуля с регистром sec_kp.
+									  //Операция assign не может быть выполнена в поведенческом always блоке.
+								  
+	//Поведенческий always блок. Только в нем возможно использованием ветвлений и циклов.
+	always @(posedge clk) //Источник тактового сигнала установлен на плате, частота 50МГц.
 	begin
-		if (!reset)
-		begin
-			clk_number <= 25'd0;
-			sec_kp <= 1'd0;
-		end
+		if (!reset) //Кнопка сброса также уже смонтирована на CoreEP4CE10, инверсная логика.
+			begin
+				clk_number <= 25'd0;
+				sec_kp <= 1'd0; //Сброс регистров в начальное (нулевое) состояние.
+									 //Неблокирующее присваивание "<=". Присвоение регистров произойдет одновременно, параллельно.
+									 //Формат 1'd0: число до апострофа указывает на то сколько бит необходимо для хранения константы,
+									 //буква после апострофа говорит о записи идущего следом числа (b - двоичная, d - десятичная и т.д.)
+			end
+		else			//Если нога сброса подтянута к Vcc (не нажата кнопка), находится в z состоянии
+						//или даже в неопределенном (x). В общем там не ноль, то...
+			begin
+				if (clk_number <= 25'd25000000) 		//Проверка значения счетчика тактов, если не отсчитано 25 миллионов,
+					clk_number <= clk_number + 25'd1; //то продолжить отсчет
+				else
+					clk_number <= 25'd0; //иначе счетчик заполнен, сбросить его в ноль.
 	
-		if (clk_number <= 25'd25000000) //Equal or +1? No care. Synthsizer is smart.
-			clk_number <= clk_number + 25'd1;
-		else
-			clk_number <= 25'd0;
-	
-		if (clk_number > 25'd12500000) 
-			sec_kp <= 1'd1;
-		else
-			sec_kp <= 1'd0;
+				if (clk_number > 25'd12500000) //Период сигнала здесь пол-секунды.
+					sec_kp <= 1'd1; //Четверть секунды удерживается единица.
+				else
+					sec_kp <= 1'd0; //Вторую четверть - ноль.
+			end
 			
-	end
-	
-	assign time_out = sec_kp;
+	end	
 	
 endmodule
--- Svetofors_025/tri_light_svetofor.v
+++ Svetofors_025/tri_light_svetofor.v
@@ -1,66 +1,81 @@
-module tri_light_svetofor ( //Блок управления тремя светдиодами. Для каждого светодиода определен
-//свой временной интервал свечения. Подобно светофору.
-		//A three LEDs control module. The each LED have a special lighting time.
-//Like a usual three color stoplight.
+module tri_light_svetofor (	//Этот модуль описывает логику имитации трехцветного светофора.
+ //A usual tricolor traffic light. Inverter logic, 1 is off.
 		input wire time_signal,
 		input wire reset,
 		output wire red,
 		output wire yellow,
 		output wire green		
 		);
-		
-//Параметры - количество тактов. Такт - пол секунды. Секунда - два такта.
-	//Для удобства время задается как произведение на два.
+	//Блок параметров - аналог констант в Verilog. Описываются временные интервалы
+	//каждого из режимов работы светофора, для проверок в блоке always.
 	parameter green_time = 8'd26 * 8'd2,
 	red_time = 8'd84 * 8'd2,
 	yellow_time = 8'd3 * 8'd2,
 	blnk_time = 8'd4 * 8'd2,
-//Общая продолжительность работы 
 	tril_time = green_time + blnk_time + red_time + yellow_time * 8'd2;
 	
-	reg [7:0] timer; //A time keeper for this module.
-	reg [2:0] sel_color; //A output selector: 0 bit - red, 1 bit - yellow, 2 bit - green
+	reg [7:0] timer; 	//Счетчик сигналов времени.
+							//A time keeper for this module.
+	reg [2:0] sel_color; //Регистр выбора цвета светофора. 0 бит - красный, 1 бит - желтый, 2 бит - зеленый.
+								//Активный сигнал для светодиодов - ноль.
+								//A output selector: 0 bit - red, 1 bit - yellow, 2 bit - green. A zero level is active.
 	
-	always @(posedge time_signal or negedge reset)
-	begin
-		if (!reset)
-		begin
-			timer = 8'd0;
-			sel_color = 3'b110;
-		end
-		else
-		begin
-			if (timer < tril_time) //Increment and
-				timer = timer + 8'd1;
-			else
-				timer = 8'd0; //reset timer.
-			
-			if (timer <= red_time) //A red color time.
-				sel_color = 3'b110;
-							
-			//A yellow color time.
-			if (timer > red_time && timer <= red_time + yellow_time)
-				sel_color = 3'b100;
-		
-			//A green color time.
-			if (timer > red_time + yellow_time && timer <= red_time + yellow_time + green_time)
-				sel_color = 3'b011;
-			
-			if (timer > red_time + yellow_time + green_time && timer <= red_time + yellow_time + green_time + blnk_time)
-				if (sel_color == 3'b011) //A green blinking.
-					sel_color = 3'b111;
-				else
-					sel_color = 3'b011;
-		
-			//A yellow color time.
-			if	(timer > red_time + yellow_time + green_time + blnk_time) //End of sequence. Next - timer reset.
-				sel_color = 3'b101;
-		end
-		
-	end
-	
+	//Соединение выходных проводов модуля с соотвествующими битами регистра выбора цвета.
+	//Операция assign должна выполняться всегда и поэтому не может размещаться в поведенческом блоке always.
 	assign red = sel_color[0];
 	assign yellow = sel_color[1];
 	assign green = sel_color[2];
+	
+	//Поведенческий блок always, выполняется по сигналам (по умолчанию - фронт) перечисленным в скобках.
+	//Только здесь возможно использование ветвлений и циклов verilog.
+	always @(posedge time_signal or negedge reset)
+	begin
+		//Здесь в операциях с регистрами используется неблокирующее присваивание "<=".
+		//Эти операции не выполняются в месте использования, а откладываются до конца блока always.
+		//Зато в конце блока они выполняются все разом, одновременно. Абсолютно ПАРАЛЛЕЛЬНО.
+		if (!reset) //Если нажата кнопка сброса (активный сигнал - ноль), то...
+			begin
+				timer <= 8'd0;
+				sel_color <= 3'b110; //Произвести установку регистров в начальное состояние.
+											//Работа светофора начинается с красного сигнала. Активный сигнал - ноль.
+			end
+		else //Иначе, вход сброса не в нулевом состоянии (там единица, z или вообще неопределенное состояние).
+			begin //Ниже идет логика работы светофора.				
+				if (timer < tril_time)	//Если значение счетчика не добралось до конца одного цикла работы светофора... 
+											//Increment and
+					timer <= timer + 8'd1; //Увеличить его еще на единицу.
+				else
+					timer <= 8'd0; 	//Иначе начать отсчет заново.
+											//reset timer.
+			
+				if (timer <= red_time) //Если значение таймера лежит в интервале работы красного света.
+											//A red color time.
+					sel_color <= 3'b110; //Включить красный (активный - ноль), а желтый и зеленый отключить.
+							
+				//A yellow color time.
+				if (timer > red_time && timer <= red_time + yellow_time) //Желтый свет - переход от красного к зеленому.
+					sel_color <= 3'b100; //Горят два светодиода.
+		
+				//A green color time.
+				if (timer > red_time + yellow_time && timer <= red_time + yellow_time + green_time) //Зеленый свет.
+					sel_color <= 3'b011; //Горит один светодиод, красный и желтый выключены.
+				
+				//Окончание работы зеленого света.
+				//На протяжении этого интервала зеленый сигнал мигает с частотой сигнала времени.
+				if (timer > red_time + yellow_time + green_time && timer <= red_time + yellow_time + green_time + blnk_time)
+					if (sel_color == 3'b011)//A green blinking.
+						sel_color <= 3'b111; //Выключить зеленый.
+					else
+						sel_color <= 3'b011; //Включить зеленый.
+		
+				//A yellow color time.
+				//End of sequence. Next - timer reset.
+				//Переход от зеленого сигнала к красному. Конец цикла работы светофора.
+				//Дальше будет сброс таймера и все заново.
+				if	(timer > red_time + yellow_time + green_time + blnk_time)
+					sel_color <= 3'b101; //Горит один светодиод - желтый. 
+			end
+		
+	end	
 		
 endmodule
